42d577c13152f02ac800ff43532c5effef876936
ephys <ephyspotato@gmail.com>
2014-08-06 11:28:01 +0200

    changed forge version

--- src/main/java/nf/fr/ephys/omnifluids/Omnifluids.java
+++ src/main/java/nf/fr/ephys/omnifluids/Omnifluids.java
@@ -9,7 +9,7 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import nf.fr.ephys.omnifluids.common.core.CommonProxy;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = Omnifluids.MODID, version = Omnifluids.VERSION, name = Omnifluids.NAME, dependencies = "after:*;required-after:cookiecore")
+@Mod(modid = Omnifluids.MODID, version = Omnifluids.VERSION, name = Omnifluids.NAME, dependencies = "after:*;required-after:cookiecore;required-after:Forge@[10.13.0.1188,)")
 public class Omnifluids {
     public static final String MODID = "omnifluids";
     public static final String VERSION = "1.1.0";
